e6159daeb69d96a4cf7fcca8628d9effbff2d7a3
Raj kumar bhootla <bhootlarajkumar@gmail.com>
2025-09-29 20:19:11 +0530

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineNuxtConfig({
+//   compatibilityDate: "2025-07-15",
+//   devtools: { enabled: true },
+
+//   css: [
+//     "~/assets/css/main.css",
+//     "@fortawesome/fontawesome-free/css/all.css",
+//     "@fortawesome/fontawesome-free/css/all.min.css",
+//   ],
+
+//   vite: {
+//     plugins: [tailwindcss()],
+//   },
+//   app: {
+//     head: {
+//       link: [
+//         {
+//           rel: "stylesheet",
+//           href: "https://fonts.googleapis.com/css2?family=Orbitron:wght@700&family=Russo+One&family=Audiowide&family=Exo+2:wght@600;700;800&display=swap",
+//         },
+//       ],
+//     },
+//   },
+//   modules: [
+//     "@nuxt/content",
+//     "@nuxt/eslint",
+//     "@nuxt/image",
+//     "@nuxt/scripts",
+//     "@nuxt/test-utils",
+//     "@nuxt/ui",
+//     "@pinia/nuxt",
+//   ],
+// });
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  ssr: true, // ✅ enable server-side rendering
+  nitro: {
+    preset: "vercel", // ✅ make Nuxt use Vercel serverless functions
+  },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
